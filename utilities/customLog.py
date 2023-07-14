09c1cb1a61e057d196dc5af511d41183618f1bdd
AmadouDiallo4 <diallobis2011@gmail.com>
2023-07-14 20:24:14 +0200

    add comment for logging code

--- utilities/customLog.py
+++ utilities/customLog.py
@@ -5,10 +5,15 @@ class logGen():
 
     @staticmethod
     def loggen():
+        # getLogger() method takes the test case name as input
         logger = logging.getLogger("Test Login")
+        # FileHandler() method takes location and path of log file
         fileHandler = logging.FileHandler('.//Logs//automation.log')
+        # Formatter() method takes care of the log file formatting
         formatter = logging.Formatter("%(asctime)s : %(levelname)s :%(name)s :%(message)s")
+        # addHandler() method takes fileHandler object as parameter
         fileHandler.setFormatter(formatter)
         logger.addHandler(fileHandler)
+        # setting the logger level
         logger.setLevel(logging.INFO)
         return logger
